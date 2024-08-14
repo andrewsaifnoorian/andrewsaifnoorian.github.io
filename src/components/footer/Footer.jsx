910404d5128d2c402311cecc12f3dc85891db8a8
Andrew Safe <andrewsafe@gmail.com>
2024-08-13 22:05:43 -0400

    back to top

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,6 +6,7 @@ import {FaGithub, FaLinkedin, FaFileAlt} from 'react-icons/fa'
 const Footer = () => {
   return (
     <footer>
+      <a href="#" className='footer_logo'>Back to the top</a>
       <ul className='permalinks'>
         <li><a href='#'>Home</a></li>
         <li><a href='#about'>About</a></li>
