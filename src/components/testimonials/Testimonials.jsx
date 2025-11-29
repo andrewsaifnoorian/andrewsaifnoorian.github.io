ad1066d8d2c66a9c4988077d96ea99536a643fb7
andrewsafe <andrewsafe@gmail.com>
2025-11-29 09:47:44 -0500

    Final updates before merging fix2 into main

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import "./testimonials.css";
 import AVTR1 from "../../assets/psmelvinson.png";
-import AVTR2 from "../../assets/amavs.png";
-import AVTR3 from "../../assets/mahdyf.jpg";
-import AVTR4 from "../../assets/tnoble.jpg";
-import AVTR5 from "../../assets/shorrigan.png";
+import AVTR2 from "../../assets/mahdyf.jpg";
+import AVTR3 from "../../assets/tnoble.jpg";
+import AVTR4 from "../../assets/shorrigan.png";
 import { Navigation } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -15,31 +14,25 @@ const peers = [
     avatar: AVTR1,
     name: "Pawel Morysewicz",
     review:
-      "Andrew is one of the best teammates I have ever worked with, he's a great listener, fast learner that provides help to anyone in the team if needed, and is very committed to the responsibilities that he has.",
+      "Andrew is the best teammate I have ever worked with in and out of work.",
   },
   {
     avatar: AVTR2,
-    name: "Andrew Mavrogeorgis",
-    review:
-      "I had the chance to work with Andrew on different projects. His technical skills were always impressive. He constantly showed professional behavior and high level of problem-solving skills. Based on that, I would gladly recommend him as a software developer with his technical and soft skills.",
-  },
-  {
-    avatar: AVTR3,
     name: "Mahdy Ferdaos",
     review:
       "Working alongside Andrew has been great. His learning is exponential. He is always eager to learn and improve his skills.",
   },
   {
-    avatar: AVTR4,
+    avatar: AVTR3,
     name: "Tyler Noble",
     review:
       "Andrew is next up in cyber. He is a great team player and has a strong work ethic.",
   },
   {
-    avatar: AVTR5,
+    avatar: AVTR4,
     name: "Samuel Horrigan",
     review:
-      "There is no one that has more conviction than Andrew when it comes to data structures and algorithms.",
+      "There is no one that has more conviction than Andrew when it comes to data structures, algorithms, and system design.",
   },
 ];
 
