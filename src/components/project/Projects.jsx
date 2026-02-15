2f7decac70920e7046deafaf5705e88730e70b62
andrewsafe <andrewsafe@gmail.com>
2026-02-15 15:54:33 -0500

    Migrate from Create React App to Vite + React 19
    Remove unused deps (react-pdf, three.js ecosystem), upgrade all
    dependencies to latest, switch build tooling to Vite for faster
    dev/build times, and fix Swiper v11 imports.

--- src/components/project/Projects.jsx
+++ src/components/project/Projects.jsx
@@ -1,59 +1,75 @@
 import React from "react";
 import "./project.css";
-import IMG1 from "../../assets/heroesLLCpic.png";
-import IMG2 from "../../assets/primedtNJ.png";
-import IMG3 from "../../assets/cluegame.png";
-import IMG4 from "../../assets/poker5hand.png";
-import IMG5 from "../../assets/underMSRP.png";
-import IMG6 from "../../assets/CarM.png";
-import IMG7 from "../../assets/tasksapp.png";
+import IMG1 from "../../assets/primedtNJ.png";
+import IMG2 from "../../assets/NN-Final-Project.png";
+import IMG3 from "../../assets/devopsCourseProject.png";
+import IMG4 from "../../assets/heroesLLCpic.png";
+import IMG5 from "../../assets/cluegame.png";
+import IMG6 from "../../assets/poker5hand.png";
+import IMG7 from "../../assets/underMSRP.png";
+import IMG8 from "../../assets/CarM.png";
+import IMG9 from "../../assets/tasksapp.png";
 
 const projects = [
   {
     id: 1,
     image: IMG1,
-    title: "Heroes Movement LLC",
-    github: "",
-    demo: "https://theheromovement.org/",
-  },
-  {
-    id: 2,
-    image: IMG2,
     title: "Prime Detailing NJ",
     github: "",
     demo: "https://primedetailing.biz/",
   },
   {
+    id: 2,
+    image: IMG2,
+    title: "Targeted Advertising ROI Classification Using Neural Networks",
+    github: "",
+    demo: "https://www.linkedin.com/in/andrewsaifnoorian/overlay/1765563886158/single-media-viewer?type=DOCUMENT&profileId=ACoAACgPUV8BK03EPpwlXxmLS4F0QPrg6X4E83w&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3Bnc6uSRWKRRW5rZp8o2R%2F%2Fg%3D%3D",
+  },
+  {
     id: 3,
     image: IMG3,
+    title: "DevOps & Secure Software Development Project",
+    github: "",
+    demo: "https://drive.google.com/file/d/1qtxVQO15Utn3ut901Ol_eHmz8_zGC-K_/view?usp=drivesdk",
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "Heroes Movement LLC",
+    github: "",
+    demo: "https://theheromovement.org/",
+  },
+  {
+    id: 5,
+    image: IMG5,
     title: "Clue Game",
     github: "https://github.com/andrewsafe/clue-game",
     demo: "https://67565c59e06325000836e7ec--clue-game.netlify.app/",
   },
   {
-    id: 4,
-    image: IMG4,
+    id: 6,
+    image: IMG6,
     title: "5 Hand Poker",
     github: "https://github.com/andrewsafe/project2_asaifnoorian",
     demo: "",
   },
   {
-    id: 5,
-    image: IMG5,
+    id: 7,
+    image: IMG7,
     title: "Under MSRP App",
     github: "https://github.com/andrewsafe/BulkEmails",
     demo: "https://undermsrp.netlify.app/",
   },
   {
-    id: 6,
-    image: IMG6,
+    id: 8,
+    image: IMG8,
     title: "Car Maintenance App",
     github: "https://github.com/SKhan2001/CarTracker",
     demo: "",
   },
   {
-    id: 7,
-    image: IMG7,
+    id: 9,
+    image: IMG9,
     title: "Tasks Web App",
     github: "https://github.com/andrewsafe/tasks-app",
     demo: "",
